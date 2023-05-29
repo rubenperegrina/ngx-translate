27f90b359ca1a97f48c8987cfabe144b972595d1
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-05-29 10:33:21 +0200

    Add translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngx-translate';
+  currentLang!: string;
+  constructor(private translate: TranslateService){
+    this.currentLang = translate.currentLang || translate.getDefaultLang();
+  }
+
+  changeLang() {
+    this.currentLang === 'es' ? this.translate.use('en') : this.translate.use('es');
+    this.currentLang = this.translate.currentLang;
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
 
+function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+      HttpClientModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          deps: [HttpClient],
+          useFactory: HttpLoaderFactory
+        },
+        defaultLanguage: 'es'
+      })
+    ])
+  ]
 };
